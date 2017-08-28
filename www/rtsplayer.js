005bf06c619423167f7696d71336d6e15ead1e20
Yossi <saoron@gmail.com>
2017-08-28 17:40:09 +0300

    revert typing for now

--- www/rtsplayer.js
+++ www/rtsplayer.js
@@ -1,12 +1,8 @@
 var exec = require('cordova/exec');
-var pluginName = 'rtsPlayer';
 
 
-function rtsPlayer() {}
-
-
-rtsPlayer.prototype.watchVideo = function(moviePath, success, error) {
+exports.watchVideo = function(moviePath, success, error) {
     exec(success, error, "rtsplayer", "watchVideo", [moviePath]);
 };
 
-module.exports = new rtsPlayer();
+
